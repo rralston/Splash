182f88375df6fc3a238cb1c68f3b86f76a60ce4a
Rupert Ralston <rralston@gmail.com>
2014-04-24 14:58:28 +1200

    Asset Precompile Enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Filmzu::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.js home.js script.js smoothscroll.js application.css home.css.scss lessframework4.css mobile.css style.css)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
